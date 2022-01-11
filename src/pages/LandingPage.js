b6f9d1c619216cfc81fc48825ea5fabd46683b35
karine <karine.pistili@gmail.com>
2022-01-11 17:10:49 -0300

    implemented landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,19 +2,21 @@ import '../styles/LandingPage.css';
 
 export default function LandingPage() {
   return (
-    <main>
+    <div>
       <div class="bg-img">
         <div class="container">
-          <div class="row">
-            <div class="col-6">
-              <h1 class="main-title">Life Planner</h1>
-            </div>
-            <div class="col-6">
-              <p class="short-paragraph">Lorem ipsum</p>
-            </div>
+          <h1 class="main-title">Life Planner</h1>
+          <div class="content-side">
+            <p class="short-paragraph">Tired of not having control over your life? <br></br>Change it!</p>
+            <button class="btn">
+                <i class="mdi mdi-google btn-icon"></i>
+                <span class="btn-icon-text">
+                  Sign in with Google
+                </span>
+            </button>
           </div>
-        </div>
+          </div>
       </div>
-    </main>
+    </div>
   );
 }
